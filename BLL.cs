a3e6a26b20b7751e83ad6fd147bc11b35ace67ad
Kumari <2064588@cognizant.com>
2022-08-29 20:40:04 +0530

    first commit

---
+++ BLL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class BLL : DAL1
+    {
+        /**public int UserStoryId { get; set; }
+        public int UserStoryName { get; set; }
+        public int StoryOwner { get; set; }
+        public int StoryPoints { get; set; }
+        public int StoryDescription { get; set; }**/
+        static void Main(string[] args)
+        {
+            DAL1 dal = new DAL1();
+            dal.RetrieveData();
+
+
+        }
+
+
+    }
+}
